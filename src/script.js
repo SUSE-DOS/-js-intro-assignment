8785f8d9609c8e4065768f44009f1d7985d6ea70
jsimpsonux <86470253+jsimpsonux@users.noreply.github.com>
2021-07-10 11:05:35 +0100

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,10 @@
+const celsius = window.prompt("Enter your celsius: ");
+
+const celsiusToFahrenheit = celsius * 1.8 + 32;
+const celsiusToKelvin = (celsius + 459.67) / 1.8;
+
+console.log(`Celsius: #{celsiusToFahrenheit}`);
+console.log(`Fahrenheit: #{...}`);
+console.log(`Kelvin: #{celsiusToKelvin}`);
+
+///https://www.mikedane.com/web-development/javascript/getting-user-input/
